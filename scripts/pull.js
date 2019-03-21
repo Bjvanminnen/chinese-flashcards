c7763adc381b6374f3f629ee0ddb10f226f0c610
Brent Van Minnen <bjvanminnen@gmail.com>
2019-03-21 11:55:23 -0700

    push and pull

---
+++ scripts/pull.js
@@ -0,0 +1,36 @@
+const fs = require('fs').promises;
+const { google } = require('googleapis');
+const authorize = require('./auth');
+
+const SHEET_ID = '1K6u7y-Cjdkt43tzmNzgem5xJJTJNM71-SWny1CEOAJU';
+
+async function pull() {
+  const auth = await authorize();
+  const sheets = google.sheets({version: 'v4', auth});
+
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: SHEET_ID,
+    range: 'Sheet1',
+  });
+  const [headers, ...characters] = response.data.values;
+
+  const data = {
+    headers,
+    characters
+  };
+  await fs.writeFile('../src/characters.json', customStringify(data));
+  console.log('Finished');
+}
+pull();
+
+function customStringify(data) {
+  const arrayStr = rg => `[${rg.map(text => `"${text}"`).join(', ')}]`;
+
+  return `
+{
+  "headers": ${arrayStr(data.headers)},
+  "characters": [
+    ${data.characters.map(row => arrayStr(row)).join(',\n    ')}
+  ]
+}`;
+}
--- scripts/push.js
+++ scripts/push.js
@@ -1,21 +1,24 @@
 const { google } = require('googleapis');
-
+const characters = require ('../src/characters');
 const authorize = require('./auth');
+
+const SHEET_ID = '1K6u7y-Cjdkt43tzmNzgem5xJJTJNM71-SWny1CEOAJU';
 
 async function push() {
   const auth = await authorize();
-
   const sheets = google.sheets({version: 'v4', auth});
-  sheets.spreadsheets.values.get({
-    spreadsheetId: '1K6u7y-Cjdkt43tzmNzgem5xJJTJNM71-SWny1CEOAJU',
-    range: 'Sheet1',
-  }, (err, res) => {
-    if (err) {
-      console.error(err);
-      return;
+
+  const response = await sheets.spreadsheets.values.update({
+    spreadsheetId: SHEET_ID,
+    range: 'A1',
+    valueInputOption: 'RAW',
+    resource: {
+      values: [characters.headers].concat(characters.characters)
     }
-    console.log('----');
-    console.log(res);
   });
+  console.log(response.data);
+
+
+
 }
 push();
