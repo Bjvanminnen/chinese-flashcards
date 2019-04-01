eab6fa2338a844f674e36c04c3b5fbb1e0a4c1a6
Brent Van Minnen <bjvanminnen@gmail.com>
2019-04-01 13:57:33 -0700

    stop stashing

--- sync-sheet.sh
+++ sync-sheet.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+# TODO: could make this more resilient to any local changes
+
 git pull
-git stash
 npm run pullchars
 git add src/characters.json
 git commit -m "Characters auto-update"
 git push
-git stash pop
